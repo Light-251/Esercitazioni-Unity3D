f5573e2aabbae1616a3e1c094db3ee76e4a2335c
SLP <79428202+Light-251@users.noreply.github.com>
2023-02-27 22:59:52 +0100

    Add files via upload

---
+++ Prove2D/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    Transform player;
+
+    Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    float z_offset = 10f;
+    [SerializeField]
+    float y_offset = 3f;
+    [SerializeField]
+    float smooth = 0.3f;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Giocatore").transform;
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector3 targetPosition = new Vector3(player.position.x, player.position.y + y_offset, player.position.z - z_offset);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smooth);
+        }
+        
+    }
+}
---
+++ Prove2D/Assets/Scripts/Movement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 3f;
+    Rigidbody2D body;
+    Animator anim;
+    bool isJumping = false;
+    [SerializeField]
+    float jumpForce = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        Jumping();
+    }
+
+    void PlayerMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector2 velocity = new Vector2(Vector2.right.x * (moveSpeed * h), body.velocity.y);
+        body.velocity = velocity;
+
+        anim.SetFloat("Speed", velocity.x);
+
+        if (velocity.x < 0)
+        {
+            anim.transform.localScale = new Vector3(-1, 1, 1);
+            body.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (velocity.x > 0)
+        {
+
+            body.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    void Jumping()
+    {
+        if (isJumping)
+        {
+            if (body.velocity.y == 0)
+            {
+                isJumping = false;
+                anim.SetBool("isJumping", false);
+            }
+        }
+        else
+        {
+            if (Input.GetAxis("Jump") > 0)
+            {
+                body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                isJumping = true;
+                anim.SetBool("isJumping", true);
+            }
+        }
+    }
+}
